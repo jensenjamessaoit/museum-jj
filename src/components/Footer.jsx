aff3394c1191bb11fc828551f24685c82e99347e
jj <saoitjensen@gmail.com>
2023-11-30 03:25:59 -0700

    Homepage, footer, and icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,27 @@
+import { FaGithub } from "react-icons/fa";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="container" style={{ width: "50%" }}>
+        <div className="mx-auto">
+          <a
+            href="https://github.com/jensenjamessaoit"
+            style={{ color: "black", textDecoration: "none" }}
+          >
+            github
+            <FaGithub
+              style={{
+                height: "20px",
+                width: "20px",
+                color: "#000000",
+              }}
+            />
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,34 @@
 const Home = () => {
   return (
     <>
-      <div>Hello</div>
+      <div className="container-fluid" style={{ height: "80vh" }}>
+        <div
+          className="row justify-content-center align-items-center"
+          style={{ height: "100%" }}
+        >
+          <div className="card mx-auto" style={{ width: "90%", height: "98%" }}>
+            <div className="card-body">
+              <h1 className="card-title">Welcome to my Museum</h1>
+              <h3 className="card-subtitle">By JJ Saoit</h3>
+              <p>
+                Passionate web developer with a keen enthusiasm for the
+                limitless possibilities of the web. Skilled in managing and
+                creating both relational (MySQL) and non-relational (MongoDB)
+                back-end/server-side databases. Experienced in developing
+                dynamic and responsive user interfaces using front-end
+                frameworks like React. I&apos;ve successfully completed projects
+                ranging from a weather dashboard that utilizes the openweather
+                API to a web app that helps Utah real estate lawyers cite laws
+                much more efficiently, where I utilized technologies such as
+                express, graphql, apollo, mongoose, and react. With a dedication
+                to clean and efficient code, I believe in the transformative
+                power of technology to elevate user experiences and solve
+                real-world problems.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
